a4eb3f78107418426eb60f0bace04c42a5fba527
Kamil Fatekhov <113035726+Kamil-Fatekhov@users.noreply.github.com>
2023-03-01 22:46:11 +0300

    Update matrix.c

--- Fatekhov K.G/Practice2_0/Practice2_0/Project6/matrix.c
+++ Fatekhov K.G/Practice2_0/Practice2_0/Project6/matrix.c
@@ -1,75 +1,98 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "matrix.h"
-void allocate_matrix(TMatrix** matrix, int n) {
-	(*matrix) = (TMatrix*)malloc(sizeof(TMatrix));
-	(*matrix)->n = n * n;
-	(*matrix)->x = (int*)malloc(sizeof(int) * (*matrix)->n);
-}
-void fill_matrix(TMatrix* matrix) {
-	for (int i = 0; i < matrix->n; i++) {
-		 scanf_s("%d", &(matrix->x[i]));
 
-	}
+void allocate_matrix(TMatrix** matrix, int n)
+{
+	(*matrix) = (TMatrix*)malloc(sizeof(TMatrix) * 1);
+	(*matrix)->n = n*n;
+	(*matrix)->x = (int*)malloc(sizeof(int) * n * n);
 }
-void print_matrix(TMatrix* matrix,int n) {
-		int i = 0, j = 0;
+
+void fill_matrix(TMatrix* matrix, int n)
+{
+	int i = 0;
+	for (; i < n; i++) {
+		int j = 0;
 		for (; j < n; j++) {
-			for (i=0; i < n; i++) {
-				printf("%d\t",matrix->x[j * n + i]);
-			}
-			printf("\n");
+			scanf("%d", &(matrix->x[i * n + j]));
 		}
 	}
-TMatrix* plus_const(TMatrix* matrix,int constant, int n) {
-	TMatrix* res;
-	allocate_matrix(&res, matrix->n);
-	for (int i = 0; i < matrix->n; i++) {
-		res->x[i] =matrix->x[i] + constant;
-	}
-	print_matrix(res, n);
 	printf("\n");
 }
-TMatrix* multiplied_const(TMatrix* matrix, int constant,int n) {
-	TMatrix* res;
-	allocate_matrix(&res, matrix->n);
-	for (int i = 0; i < matrix->n; i++) {
-		res->x[i] =matrix->x[i] * constant;
-	}
-	print_matrix(res, n);
-	printf("\n");
-}
-TMatrix* complition_matrix(TMatrix* matrix, TMatrix* matrix2,int n) {
-	TMatrix* res;
-	allocate_matrix(&res, n);
-	for (int i = 0; i < matrix->n; i++) {
-		res->x[i]=matrix->x[i]+matrix2->x[i];
-	}
-	print_matrix(res, n);
-	printf("\n");
-}
-TMatrix* multiplication_matrix(TMatrix* matrix, TMatrix* matrix2, int n) {
-	TMatrix* res;
-	allocate_matrix(&res, n);
-	int flag = 0,j=0,i=0,count=0,countj=0,counti=0;
-	for (count; count < matrix->n; count++) {
-		flag = 0;
-		if (j  == n ) {
-			j = 0; countj = 0;
-			counti++;
-			i = n * counti;
+
+void print_matrix(TMatrix* matrix, int n)
+{
+	int j = 0;
+	for (; j < n; j++)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			printf("%d ", matrix->x[j * n + i]);
 		}
-		res->x[count] = 0;
-		while (flag < n) {
-			res->x[count] += matrix->x[i] * matrix2->x[j];
-			j += n; i++;
-			flag++;
-		}countj++; j = countj;  i = n*counti;
+		printf("\n");
 	}
-	print_matrix(res, n);
 	printf("\n");
 }
-void free_matrix(TMatrix** matrix) {
+
+void free_matrix(TMatrix** matrix)
+{
 	free((*matrix)->x);
 	free(*matrix);
+}
+
+TMatrix* add_matrix(TMatrix* matrix1, TMatrix* matrix2, int n)
+{
+	TMatrix* res;
+	int i = 0;
+	allocate_matrix(&res, n);
+	for (; i < res->n; i++)
+	{
+		res->x[i] = matrix1->x[i] + matrix2->x[i];
+	}
+	return res;
+}
+
+TMatrix* add_const(TMatrix* matrix, int n)
+{
+	TMatrix* res;
+	int i = 0;
+	allocate_matrix(&res, matrix->n);
+	for (; i < res->n; i++)
+	{
+		res->x[i] = matrix->x[i] + n;
+	}
+	return res;
+}
+
+TMatrix* multi_const(TMatrix* matrix, int n)
+{
+	TMatrix* res;
+	int i = 0;
+	allocate_matrix(&res, matrix->n);
+	for (; i < res->n; i++)
+	{
+		res->x[i] = matrix->x[i] * n;
+	}
+	return res;
+}
+
+TMatrix* multi_matrix(TMatrix* matrix1, TMatrix* matrix2, int n)
+{
+	TMatrix* res;
+	int  k = 0;
+	allocate_matrix(&res, matrix1->n);
+	int i = 0, j = 0, flag = 0;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			res->x[i*n+j] = 0;
+			for (k = 0; k < n; k++)
+			{
+				res->x[i*n+j] += matrix1->x[i * n + k] * matrix2->x[k * n + j];
+			}
+		}
+	}
+	return res;
 }
