10615875f7a2d63ea45d3769dd4e807dc5ad3b5d
Kamil Fatekhov <113035726+Kamil-Fatekhov@users.noreply.github.com>
2023-05-04 10:50:23 +0300

    Update main.c

--- FatekhovKG/Practice2_1/Practice2_1/main.c
+++ FatekhovKG/Practice2_1/Practice2_1/main.c
@@ -6,5 +6,8 @@ void main() {
 	int length = count_shops(file,adress);
 	Shop* shop = our_shop(adress, length);
 	print_struct(shop, length);
+	shop[0].op[0].open.hours = "00";
+	printf("%s", shop[0].op[0].open.hours);
 	
 }
+
