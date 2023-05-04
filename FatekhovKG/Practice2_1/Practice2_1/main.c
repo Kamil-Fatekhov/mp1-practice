88841dfca7b4bd5c496350d2367da60c71976d4c
Kamil Fatekhov <113035726+Kamil-Fatekhov@users.noreply.github.com>
2023-05-04 09:23:32 +0300

    Update main.c

--- FatekhovKG/Practice2_1/Practice2_1/main.c
+++ FatekhovKG/Practice2_1/Practice2_1/main.c
@@ -1,32 +1,10 @@
 #include "func.h"
-int main() {
-	Shop* shop;
-
+void main() {
 	FILE* file = NULL;
 	char* adress = "shops.txt";
-	int i = 0;
-	int num = 7;
-	int records = 0;
-	int* index;
 	file = fileop(adress);
-	int length = count_shops(file, adress);
-	//printf("%d", length);
-	malloc_struct(&shop, length);
-	malloc_str(shop, length);
-	records = full_struct(shop, file, num);
-	print_struct(shop, records);
-	i = quantity(shop, records);
-	malloc_array(&index, i);
-	//printf("Size %d\n", i);
-	index = full_our_shop(shop, records, i);
-	Shop* our_shop = our(shop, i, records);
-	printf("\nSituable stores: \n");
-	print_struct(our_shop, i);
-	//printf("SDS%d\t", index[2]);
-	//print_name_struct(shop, i, index);
-	free_struct(&shop);
-	free_struct(&our_shop);
-	free_array(&index);
-
-	return 0;
+	int length = count_shops(file,adress);
+	Shop* shop = our_shop(adress, length);
+	print_struct(shop, length);
+	
 }
