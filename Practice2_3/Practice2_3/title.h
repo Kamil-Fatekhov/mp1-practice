da48432a99bcaaf41856e27beaecd0db89278adb
Kamil Fatekhov <113035726+Kamil-Fatekhov@users.noreply.github.com>
2023-05-17 22:23:57 +0300

    Update title.h

--- Practice2_3/Practice2_3/title.h
+++ Practice2_3/Practice2_3/title.h
@@ -1,30 +1,87 @@
-#include <iostream>
+include <iostream>
 #include <fstream>
 #include<string>
 #include<string.h>
-#include <vector>
 using namespace std;
 #ifndef _TITLE_H
 #define _TITLE_H
-class Shops {
-private:
-	string name;
-	string adress;
-	string phone_number;
-	string specialization;
-	string opening_hours;
-	string opening_days;
-	string form_of_ownership;
+class Adress;
+class Opening_Hours;
+class Time;
+class Adress {
+	friend class List;
+	friend class Shop;
 public:
-	int fileinf;
-	Shops();
-	Shops& operator =(const Shops& shop);
-	void print();
-	friend Shops* info(int n, const string adress);
-	friend Shops* quantity(Shops* shop, int records);
+	Adress();
+	Adress& operator =(const Adress& other);
+	friend ostream& operator<<(ostream& os, const List& list);
+	string street;
+
+private:
+	string postcode;
 };
-void print_class(Shops* shop, int n);
-Shops* info(int n, const string adress);
-int file_info(const string& adress);
-Shops* quantity(Shops* shop, int records);
+class Opening_Hours {
+	friend class List;
+	friend class Time;
+	friend class Shop;
+public:
+	Opening_Hours();
+	Opening_Hours& operator = (const Opening_Hours& other);
+	friend ostream& operator<<(ostream& os, const List& list);
+
+private:
+	string Day;
+
+	class Time {
+		friend class Opening_Hours;
+		friend class List;
+		friend class Shop;
+	public:
+		friend ostream& operator<<(ostream& os, const List& list);
+		Time();
+		Time& operator = (const Time& other);
+	private:
+		string minutes;
+		string hours;
+	};
+
+	Time open;
+	Time close;
+};
+class List {
+public:
+	List& operator=(const List& other);
+	
+	friend ostream& operator<<(ostream& os, const List& list);
+	List();
+	List(const List& other);
+	~List();
+	List(string adress);
+	List correct_base();
+
+private:
+
+
+	class Shop {
+		friend class List;
+		
+	public:
+		Shop& operator =(const Shop& other);
+		Shop();
+		friend ostream& operator<<(ostream& os, const List& list);
+	private:
+		string name;
+		Adress adress;
+		string phone_number;
+		string specialization;
+		Opening_Hours* op;
+		string form_of_ownership;
+	};
+
+
+	int count;
+	Shop* shop;
+};
+
+ostream& operator<<(ostream& os, const List& list);
 #endif
